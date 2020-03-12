c3244042a840ba183c31ce4987de10e1fde1ea52
Heyward Fann <fannheyward@gmail.com>
2020-03-12 12:50:23 +0800

    feat(ci): chinese word segmentation motion

--- README.md
+++ README.md
@@ -1,14 +1,22 @@
 # coc-ci
 
-ci
+支持中文分词的 `w/b` 移动。
 
 ## Install
 
 `:CocInstall coc-ci`
+
+## Usage
+
+```vim
+nmap <silent> w <Plug>(coc-ci-w)
+nmap <silent> b <Plug>(coc-ci-b)
+```
 
 ## License
 
 MIT
 
 ---
+
 > This extension is created by [create-coc-extension](https://github.com/fannheyward/create-coc-extension)
--- src/index.ts
+++ src/index.ts
@@ -1,35 +1,61 @@
-import { CompleteOption, CompleteResult, ExtensionContext, sources, VimCompleteItem, workspace } from 'coc.nvim';
-import { Segment, useDefault } from 'segmentit';
+import { ExtensionContext, workspace } from 'coc.nvim';
+import { enPOSTag, Segment, SegmentToken, useDefault } from 'segmentit';
+
+const segmentit = useDefault(new Segment());
+
+async function fallbackMove(w = true) {
+  await workspace.nvim.eval(`feedkeys(${w ? '"w"' : '"b"'}, "n")`);
+}
+
+async function move(w = true) {
+  const { nvim } = workspace;
+  const line = await nvim.line;
+  if (!line) {
+    return fallbackMove(w);
+  }
+
+  const cursor = await nvim.eval('[line("."), col(".")]');
+  const col = cursor[1];
+
+  const lineBuf = new Buffer(line);
+  if (col >= lineBuf.length) {
+    return fallbackMove(w);
+  }
+
+  const lineBegin = lineBuf.subarray(0, col - 1).toString();
+  const lineEnd = lineBuf.subarray(col - 1).toString();
+  const parts = segmentit.doSegment(w ? lineEnd : lineBegin) as SegmentToken[];
+  if (!parts || parts.length <= 0) {
+    return fallbackMove(w);
+  }
+
+  const seg = w ? parts[0] : parts[parts.length - 1];
+  if (['un', 'nx', 'w', 'uri'].includes(enPOSTag(seg.p))) {
+    return fallbackMove(w);
+  }
+
+  const len = new Buffer(seg.w).length;
+  await nvim.call('cursor', [cursor[0], w ? col + len : col - len]);
+}
 
 export async function activate(context: ExtensionContext): Promise<void> {
-  const segmentit = useDefault(new Segment());
-
   context.subscriptions.push(
-    sources.createSource({
-      name: 'coc-ci', // unique id
-      priority: 10,
-      doComplete: async (opt: CompleteOption) => {
-        const doc = workspace.getDocument(opt.bufnr);
-        if (!doc || opt.input.length === 0) {
-          return;
-        }
+    workspace.registerKeymap(
+      ['n'],
+      'ci-w',
+      async () => {
+        await move();
+      },
+      { sync: false, cancel: true, silent: true }
+    ),
 
-        const items: VimCompleteItem[] = [];
-        const lines = await doc.buffer.lines;
-        lines.forEach(line => {
-          if (line.trim().length > 10) {
-            segmentit.doSegment(line.trim()).forEach(seg => {
-              if (seg.w && seg.w.length > 2 && /^[\u4e00-\u9FA5]+$/.test(seg.w)) {
-                items.push({ word: seg.w, menu: '[Ci]' });
-              }
-            });
-          }
-        });
-
-        return new Promise<CompleteResult>(resolve => {
-          resolve({ items });
-        });
-      }
-    })
+    workspace.registerKeymap(
+      ['n'],
+      'ci-b',
+      async () => {
+        await move(false);
+      },
+      { sync: false, cancel: true, silent: true }
+    )
   );
 }
